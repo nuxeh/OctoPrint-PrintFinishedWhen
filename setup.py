6bd7c898b7725c5141cbce986916230368a6d294
Ed Cragg <ed.cragg@eipi.xyz>
2026-01-07 17:06:02 +0000

    Bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="OctoPrint-PrintFinishedWhen",
-    version="0.1.6",
+    version="0.1.7",
     description="Sends periodic messages showing how long ago a print finished",
     long_description="A lightweight OctoPrint plugin that periodically "
                      "notifies you how long ago a print finished, with LCD M117 support.",
