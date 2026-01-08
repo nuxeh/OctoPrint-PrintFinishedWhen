91770651e9dc0d6c772d3bf5791dbbf409794c5f
Ed Cragg <ed.cragg@eipi.xyz>
2026-01-08 17:12:34 +0000

    Update metadata

--- setup.py
+++ setup.py
@@ -2,13 +2,17 @@ from setuptools import setup
 
 setup(
     name="OctoPrint-PrintFinishedWhen",
-    version="0.1.15",
-    description="Sends periodic messages showing how long ago a print finished",
-    long_description="A lightweight OctoPrint plugin that periodically "
-                     "notifies you how long ago a print finished, with LCD M117 support.",
+    version="0.2.0",
+    description="Displays how long ago a print finished on the printer LCD",
+    long_description=(
+        "A minimal OctoPrint plugin that periodically sends M117 messages "
+        "to the printer LCD showing how long ago a print finished."
+    ),
+    long_description_content_type="text/plain",
     author="Ed Cragg",
     url="https://github.com/nuxeh/OctoPrint-PrintFinishedWhen",
     license="ISC",
+    python_requires=">=3.7,<4",
     packages=["octoprint_print_finished_when"],
     include_package_data=True,
     entry_points={
