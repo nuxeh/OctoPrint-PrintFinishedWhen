21809236ab0abf3902f621ff2d309868f4cae419
Ed Cragg <ed.cragg@eipi.xyz>
2026-01-08 08:20:47 +0000

    Add logging to jsvascript

--- octoprint_print_finished_when/static/js/print_finished_when.js
+++ octoprint_print_finished_when/static/js/print_finished_when.js
@@ -1,13 +1,26 @@
 $(function () {
+    console.log("Print Finished When: JavaScript loading...");
+
     function PrintFinishedWhenViewModel(parameters) {
         var self = this;
 
+        console.log("Print Finished When: ViewModel constructor called");
+        console.log("Print Finished When: Parameters:", parameters);
+
         self.settings = parameters[0];
 
+        console.log("Print Finished When: Settings:", self.settings);
+
         self.onDataUpdaterPluginMessage = function (plugin, data) {
-            if (plugin !== "print_finished_when") return;
+            console.log("Print Finished When: Plugin message received:", plugin, data);
+
+            if (plugin !== "print_finished_when") {
+                console.log("Print Finished When: Ignoring message from other plugin");
+                return;
+            }
 
             if (data.text) {
+                console.log("Print Finished When: Showing notification:", data.text);
                 new PNotify({
                     title: "Print Finished",
                     text: data.text,
@@ -18,19 +31,32 @@ $(function () {
         };
 
         self.testNotification = function () {
-            console.log("Test notification button clicked");
+            console.log("=== Print Finished When: Test notification button clicked ===");
+            console.log("Print Finished When: Calling simpleApiCommand...");
 
             OctoPrint.simpleApiCommand(
                 "print_finished_when",
                 "test_notification"
-            );
+            ).done(function(response) {
+                console.log("Print Finished When: API call SUCCESS:", response);
+            }).fail(function(xhr, status, error) {
+                console.error("Print Finished When: API call FAILED");
+                console.error("Print Finished When: Status:", status);
+                console.error("Print Finished When: Error:", error);
+                console.error("Print Finished When: Response:", xhr.responseText);
+            });
         };
+
+        console.log("Print Finished When: ViewModel initialized successfully");
     }
+
+    console.log("Print Finished When: Registering viewmodel...");
 
     OCTOPRINT_VIEWMODELS.push({
         construct: PrintFinishedWhenViewModel,
         dependencies: ["settingsViewModel"],
         elements: ["#settings_plugin_print_finished_when"]
     });
-});
 
+    console.log("Print Finished When: Viewmodel registered");
+});
